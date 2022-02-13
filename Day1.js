c8149f6afbde1a7ca28c46972a144a1c5e278582
manjeetyadav0105 <74537892+manjeetyadav0105@users.noreply.github.com>
2022-02-14 00:11:46 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,55 @@
+var a = 20
+var b = 50
+
+// Arthmatic Operator
+var c = a+b
+var d = b - a
+var e = a * b
+var f = a/b
+var g = b % a
+console.log(c, d, e, f, g);
+
+//Assignment 
+var p = 10
+var q = p
+console.log(q)
+
+//Comparison
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+console.log(a>=b)
+
+//Identity
+console.log(a===b)
+console.log(a!==b)
+
+
+//conditional 
+if(a>b && c>>a){
+    console.log("c is largest")
+}
+else {
+    console.log("c is not largest")
+}
+
+if(a>b || c>>a){
+    console.log("a is largest")
+}
+else {
+    console.log("a is smaller")
+}
+
+if(a<b){
+    console.log(!True)
+}
+else {
+    console.log("True")
+}
+
+
+//unary
+a++
+c--
+console.log(a)
+console.log(c)
